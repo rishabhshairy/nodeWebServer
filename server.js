314bb083c0aa1cd5416e5282fc61250d8f4aee4c
rishabhshairy <rishusinha8@gmail.com>
2018-09-30 02:34:08 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express=require('express');
+var app=express(); 
+const hbs=require('hbs');
+const fs=require('fs');
+
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getCurrYear',()=>{
+     return new Date().getFullYear()
+});
+
+
+app.set('view engine','hbs');
+
+//setting loggger
+app.use((req,res,next)=>{
+    var time= new Date().toString();
+    var log=`${time} Method:- ${req.method} URL:- ${req.url}`;
+    fs.appendFileSync('server.log',log+ '\n');
+    next();
+});
+// app.use((req,res,next)=>{
+//     res.render('maintain.hbs')
+// });
+app.use(express.static(__dirname+'/public'));
+app.get('/', (req,res)=>{
+    res.render('home.hbs',{
+        title:'Home Page',
+        body:'This is the home page'
+    }); 
+});
+//dyanamic rendering of webpages 
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        title:'About Page',
+        body:'Hello this is about page'
+    });
+});
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage:'This is a bad request'
+    });
+})
+app.listen(3000,()=>{
+    console.log('server listening on port 3000');
+});
